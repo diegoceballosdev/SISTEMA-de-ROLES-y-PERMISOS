34798d7d128ef2f6d4d776b642556be6f4dd0bc8
diegoceballos95 <diegoceballos95@yahoo.com>
2024-01-31 01:03:43 -0300

    Agregado de <build> en el pom.xml

---
+++ target/generated-sources/annotations/com/mycompany/login/logica/Usuario_.java
@@ -0,0 +1,17 @@
+package com.mycompany.login.logica;
+
+import com.mycompany.login.logica.Rol;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-01-31T00:15:21", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, Rol> rolUsuario;
+    public static volatile SingularAttribute<Usuario, String> contrasenia;
+    public static volatile SingularAttribute<Usuario, Integer> id;
+    public static volatile SingularAttribute<Usuario, String> nombreUsuario;
+
+}
